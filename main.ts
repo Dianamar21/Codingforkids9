b8b652294fbb49a5a7864302f71acbad96021f27
Dianamar21 <84254354+Dianamar21@users.noreply.github.com>
2021-10-03 11:12:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
